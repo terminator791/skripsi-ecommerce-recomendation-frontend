9802051462456e9b34b65ba13fd11db7e99c230b
Mohammad Iqbal <mohammad.iqbal7781@gmail.com>
2026-05-18 22:51:01 +0700

    Add no-image placeholder images in assets and public directories
    - Introduced a new no-image placeholder PNG file in both the assets/images and public/images directories.
    - The placeholder image is intended for use when no image is available for display.

--- components/home/CategoryMenuItem.vue
+++ components/home/CategoryMenuItem.vue
@@ -1,23 +1,25 @@
 <template>
     <li class="border-b w-full">
       <a :class="{ 'active': isActive }"
-      @click.prevent="$emit('select', category.slug)" href="#" class="flex justify-between py-2 collapsed items-center text-gray-800 hover:text-green-600"
-        :data-bs-toggle="'collapse'" :data-bs-target="`#${category.slug}`" aria-expanded="false"
-        :aria-controls="category.slug">
+      @click.prevent="handleCategoryClick" href="#" class="flex justify-between py-2 items-center text-gray-800 hover:text-green-600"
+        :data-bs-toggle="hasChildren ? 'collapse' : ''" 
+        :data-bs-target="hasChildren ? `#${category.slug}` : ''" 
+        :aria-expanded="false"
+        :aria-controls="hasChildren ? category.slug : ''">
         {{ category.name }}
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-right inline-block"
+        <svg v-if="hasChildren" xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-right inline-block"
           width="14" height="14" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
           stroke-linecap="round" stroke-linejoin="round">
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
           <path d="M9 6l6 6l-6 6" />
         </svg>
       </a>
-      <div :id="category.slug" class="accordion-collapse collapse" data-bs-parent="#categoryCollapseMenu">
+      <div v-if="hasChildren" :id="category.slug" class="accordion-collapse collapse" data-bs-parent="#categoryCollapseMenu">
         <div class="text-gray-800">
           <ul class="flex flex-wrap flex-col ml-3">
-            <li v-for="(item, index) in category.childs" :key="index" >
+            <li v-for="(item, index) in category.child" :key="index" >
               <a href="#!" class="inline-block py-2 no-underline hover:text-green-600"
-              :class="{ 'active': isActive }"
+              :class="{ 'active': isChildActive(item.slug) }"
               @click.prevent="$emit('select', item.slug)">
                 {{ item.name }}
               </a>
@@ -29,7 +31,9 @@
   </template>
   
   <script setup>
-  defineProps({
+  import { computed } from 'vue'
+  
+  const props = defineProps({
     category: {
       type: Object,
       required: true
@@ -37,6 +41,27 @@
     isActive: {
       type: Boolean,
       default: false
+    },
+    activeSlug: {
+      type: String,
+      default: ''
     }
   })
+  
+  const hasChildren = computed(() => {
+    return props.category.child && props.category.child.length > 0
+  })
+  
+  const isChildActive = (slug) => {
+    return props.activeSlug === slug
+  }
+  
+  const handleCategoryClick = () => {
+    // Only emit select if category has no children
+    if (!hasChildren.value) {
+      emit('select', props.category.slug)
+    }
+  }
+  
+  const emit = defineEmits(['select'])
   </script>
